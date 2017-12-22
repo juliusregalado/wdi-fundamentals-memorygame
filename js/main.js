a25d889241c9def66ecfab814252f583640892e7
Julius Regalado <jreg123@outlook.com>
2017-12-22 03:37:59 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king"
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var createForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+};
+
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+if (cardsInPlay.length === 2) {
+	createForMatch();
+ }
+ };
+
+
+
+
+
