553407dd1bb6fd3652aea260fc9f1b7abfb9141d
Julius Regalado <jreg123@outlook.com>
2017-12-25 22:42:53 -0500

    finished the assignments but still working on bonus asignments

--- js/main.js
+++ js/main.js
@@ -33,26 +33,44 @@ var checkForMatch = function () {
 }
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute("data-id");
 	console.log("User flipped " + cards[cardId].rank);	
 	console.log("User flipped " + cards[cardId].cardImage);	
 	console.log("User flipped " + cards[cardId].suit);	
 	cardsInPlay.push(cards[cardId].rank);
-	if (cardsInPlay.length === 2){
+	this.setAttribute("src", cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
 	checkForMatch ();
-	};
+	}
 };
 
-flipCard(0);
-flipCard(2);
-
-
-/*
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match");
-}  
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
-	console.log("Sorry try again");
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {	
+	var cardElement = document.createElement("img");
+	cardElement.setAttribute("src", "images/back.png");
+	cardElement.setAttribute("data-id", i);
+	cardElement.addEventListener("click", flipCard);
+	document.getElementById("game-board").appendChild(cardElement);
+	}
 }
+
+createBoard();
+
+//Create a working reset button that resets the cards without generating new ones.
+/*
+var startButton = document.getElementById("reset-button");
+for (var i = 0; i < cards.length; i++) {
+	document.getElementById("game-board").removeChild;
+	startButton.addEventListener("click", createBoard);
+		
 */
+
+	
+
+
+
+	
+
+
+
